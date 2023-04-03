c9a40e01f0d77833248c82681a59cb8796a31672
michaelgaunt404 <mike.gaunt.404@gmail.com>
2023-04-03 11:33:32 -0700

    add targets

--- _targets.R
+++ _targets.R
@@ -29,10 +29,11 @@ list(
   tar_target(
     name = data,
     command = tibble(x = rnorm(100), y = rnorm(100))
-#   format = "feather" # efficient storage of large data frames # nolint
-  ),
-  tar_target(
+    #   format = "feather" # efficient storage of large data frames # nolint
+  )
+  ,tar_target(
     name = model,
     command = coefficients(lm(y ~ x, data = data))
   )
+  ,tar_render(analysis_icrs, "analysis/analysis_temp.rmd")
 )
